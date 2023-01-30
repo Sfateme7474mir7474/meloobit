22f0006098a8bd0a06b7e3ad91f66a365bd525ce
fatemeh <sf.mirghafouri1374@gmail.com>
2023-01-31 01:11:44 +0330

    header added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React, { Component } from "react";
-import Header from "./components/header/Header";
-import { Route, Routes } from "react-router-dom";
-import "./app.css";
-import Home from "./pages/Home";
-import LastDetail from "./components/common/LastDetail";
-import PlayList from "./pages/PlayList";
-import TopDetail from "./components/common/TopDetail";
+import React from "react";
+import Google from "./pages/Google";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="play-list" element={<PlayList />} />
-          <Route path="music/:id" element={<TopDetail />} />
-          <Route path="/last-detail/:id" element={<LastDetail />} />
-        </Routes>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+     <Google />
+    </>
+  );
+};
 
 export default App;
